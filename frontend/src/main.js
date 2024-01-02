7f08a3157e2255b4a810ffb36a76fb844869247f
tyort <neotyo1986@gmail.com>
2024-01-02 13:33:11 +0300

    Важные комментарии

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from "vue";
+import { createApp } from "vue"; // для создания нового экземпляра приложения.
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+const app = createApp(App); // корневой компонент в качестве агрумента.
 
 app.use(createPinia());
 app.use(router);
 
+// необходимо создать связку между DOM-элементом и Vue-приложением;
+// cкрипт main.js должен быть прикреплён к файлу index.html, находящемуся в корне проекта.
 app.mount("#app");

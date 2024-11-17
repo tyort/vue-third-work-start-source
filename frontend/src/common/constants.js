13409e3e53abe5c9fe1d12c6ae9c2a222260866e
tyort <neotyo1986@gmail.com>
2024-11-17 10:47:44 +0300

    Убираем пробел

--- frontend/src/common/constants.js
+++ frontend/src/common/constants.js
@@ -28,5 +28,4 @@ export const DAY_IN_SEC = HOUR_IN_SEC * 24;
 export const MONTH_IN_SEC = DAY_IN_SEC * 30;
 export const YEAR_IN_SEC = DAY_IN_SEC * 365;
 export const DAY_IN_MILLISEC = DAY_IN_SEC * SEC;
-
 export const TAG_SEPARATOR = "#";

697aa1cb686be79ec0479eb5668acd8051fef643
tyort <neotyo1986@gmail.com>
2024-12-06 18:17:29 +0300

    3.9. Подключение маршрутизатора к проекту Vue-Work

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import routes from "./routes";
 
+// Создаем экземпляр маршрутизатора
 const router = createRouter({
+  // createWebHistory - копирует стандартное поведение браузера
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 });
 
 export default router;

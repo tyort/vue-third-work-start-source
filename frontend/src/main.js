4682ea0336ebdc75b28cd9a4d687da76b8f16be3
tyort <neotyo1986@gmail.com>
2024-12-16 10:22:31 +0300

    3.19. Создание и редактирование задач в проекте Vue-Work

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { clickOutside } from "./common/directives";
 
 const app = createApp(App); // корневой компонент в качестве агрумента.
 
 app.use(createPinia());
 app.use(router);
+app.directive("click-outside", clickOutside);
 
 // необходимо создать связку между DOM-элементом и Vue-приложением;
 // cкрипт main.js должен быть прикреплён к файлу index.html, находящемуся в корне проекта.
